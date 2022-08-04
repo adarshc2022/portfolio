26be759e3d9844e7ca8c850e425818d2b0bc1b9e
adarsh <adarshchavhan@outlook.com>
2022-08-04 08:39:20 +0530

    Updates

---
+++ precache-manifest.43d614c32b51db35fef1978cb9ff9f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a13939d53cee4da40a6",
+    "url": "/portfolio/static/css/main.1fb12151.chunk.css"
+  },
+  {
+    "revision": "2a13939d53cee4da40a6",
+    "url": "/portfolio/static/js/main.2a13939d.chunk.js"
+  },
+  {
+    "revision": "37a579a80ae9e0fadb5d",
+    "url": "/portfolio/static/js/1.37a579a8.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "7cdbf259859fb050e7084b4a91d37481",
+    "url": "/portfolio/static/media/logo.7cdbf259.svg"
+  },
+  {
+    "revision": "df26510d2b3f24705e5a9f480cb6242e",
+    "url": "/portfolio/static/media/bg.df26510d.svg"
+  },
+  {
+    "revision": "5a8f3581858a58d0c7d7cefc14833da3",
+    "url": "/portfolio/index.html"
+  }
+];
